2dede07539752c09263ace11d28b2a5f4724d926
Dan Dudley <born2snipe@gmail.com>
2015-07-09 18:14:42 -0500

    added new system property flag that is apparently required by 3.3.x maven...weird.

--- integration/3.3.1/src/test/java/MavenTest.java
+++ integration/3.3.1/src/test/java/MavenTest.java
@@ -1,4 +1,9 @@
 import integration.AbstractMavenVersionSupportTestCase;
+import org.junit.BeforeClass;
 
 public class MavenTest extends AbstractMavenVersionSupportTestCase {
+    @BeforeClass
+    public static void init() {
+        multiModuleSystemProperty = true;
+    }
 }
--- integration/test-module/src/main/java/integration/AbstractMavenVersionSupportTestCase.java
+++ integration/test-module/src/main/java/integration/AbstractMavenVersionSupportTestCase.java
@@ -9,12 +9,17 @@ import java.io.File;
 import java.io.IOException;
 
 public class AbstractMavenVersionSupportTestCase {
+    public static boolean multiModuleSystemProperty = false;
+
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
     @Test
     public void shouldWork() throws IOException {
-            File folder = temporaryFolder.newFolder(String.valueOf(System.nanoTime()));
-            TestExecutor.execute(folder);
+        File folder = temporaryFolder.newFolder(String.valueOf(System.nanoTime()));
+        if (multiModuleSystemProperty) {
+            System.setProperty("maven.multiModuleProjectDirectory", folder.getAbsolutePath());
+        }
+        TestExecutor.execute(folder);
     }
 }
